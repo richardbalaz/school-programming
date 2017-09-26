824d27f5d75fed4518f7236065a79ed6ac6f040d
Richard Baláž <richard.balaz@proforce-products.com>
2017-09-26 08:38:29 +0200

    Change indentation to spaces

--- informatika/26.9.2017/12_advanced.cpp
+++ informatika/26.9.2017/12_advanced.cpp
@@ -7,25 +7,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	ifstream input(INPUT_FILE);
-	ofstream output(OUTPUT_FILE);
+    ifstream input(INPUT_FILE);
+    ofstream output(OUTPUT_FILE);
 
-	/* Check if opening failed */
-	if (!input.is_open() || !output.is_open())
-		return 1;
+    /* Check if opening failed */
+    if (!input.is_open() || !output.is_open())
+        return 1;
 
-	/* Iterate every line in input file and get the longest's length */
-	int longest_line = 0;
-	while (!input.eof()) {
-		string line;
-		getline(input, line);
-		
-		if (line.length() > longest_line)
-			longest_line = line.length();		
-	}
+    /* Iterate every line in input file and get the longest's length */
+    int longest_line = 0;
+    while (!input.eof()) {
+        string line;
+        getline(input, line);
+        
+        if (line.length() > longest_line)
+            longest_line = line.length();       
+    }
 
-	/* Write result to output file */
-	output << longest_line;
+    /* Write result to output file */
+    output << longest_line;
 
-	return 0;
+    return 0;
 }
