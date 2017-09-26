3095ef7e22074b034e9f3ef3088658b816a63c13
Richard Baláž <richard.balaz@proforce-products.com>
2017-09-26 08:36:48 +0200

    Longest line in file

---
+++ informatika/26.9.2017/12_advanced.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+
+using namespace std;
+
+#define INPUT_FILE "./input.txt"
+#define OUTPUT_FILE "./output.txt"
+
+int main(int argc, char const *argv[])
+{
+	ifstream input(INPUT_FILE);
+	ofstream output(OUTPUT_FILE);
+
+	/* Check if opening failed */
+	if (!input.is_open() || !output.is_open())
+		return 1;
+
+	/* Iterate every line in input file and get the longest's length */
+	int longest_line = 0;
+	while (!input.eof()) {
+		string line;
+		getline(input, line);
+		
+		if (line.length() > longest_line)
+			longest_line = line.length();		
+	}
+
+	/* Write result to output file */
+	output << longest_line;
+
+	return 0;
+}
